cb73ccb1341ab7b51982e667d2b26ad44e34f3f9
Federico <federico.fiorini29@gmail.com>
2016-06-20 01:27:23 +0200

    set default encoding

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 app = Flask(__name__)
 app.config.from_object('config')
 
