7983a45d6b3c2d158e22ceb8b21139df48d8d9d9
Federico <federico.fiorini29@gmail.com>
2016-07-04 20:53:38 +0200

    add cleaner description + reviews

--- app/models/cleaner.py
+++ app/models/cleaner.py
@@ -14,14 +14,21 @@ class Cleaner(db.Model):
     lastname = db.Column(db.String(50), unique=False)
     email = db.Column(db.String(50), unique=True)
     mobile_number = db.Column(db.String(15), unique=True)
+    description = db.Column(db.String(500), unique=False)
+    review_rate = db.Column(db.Numeric, unique=False)
+    last_review = db.Column(db.String(500), unique=False)
     orders = db.relationship('Order', backref='cleaner', lazy='dynamic')
     schedules = db.relationship('Schedule', backref='cleaner', lazy='dynamic')
 
-    def __init__(self, name=None, lastname=None, email=None, mobile_number=None):
+    def __init__(self, name=None, lastname=None, email=None, mobile_number=None, description=None, review_rate=None,
+                 last_review=None):
         self.name = name
         self.lastname = lastname
         self.email = email
         self.mobile_number = mobile_number
+        self.description = description
+        self.review_rate = review_rate
+        self.last_review = last_review
 
     def __repr__(self):
         return '<Cleaner %r>' % self.cleaner_id
@@ -30,6 +37,7 @@ class Cleaner(db.Model):
         try:
             db.session.add(self)
             db.session.commit()
+            self.cleaner_id
         except IntegrityError:
             return False
 
